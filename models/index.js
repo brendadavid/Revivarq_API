27407800618cf06f94853053b3383b68aa9f1da9
Jonatasvangroll <jonatas.vangroll@gmail.com>
2022-06-11 18:18:36 -0300

    Update index.js
    correção pasword admin

--- models/index.js
+++ models/index.js
@@ -33,7 +33,7 @@ const createAdminUser = (model) => {
     name: "admin",
     lastName: "master",
     email: "admin@gmail.com",
-    password: "mypassword", //123456
+    password: process.env.MYSQL_PASSWORD, //123456
     isAdmin: true,
     salt: "e53a1bf917fe9032dcb179d80cebded6be7eb62d2191d9f974f6ed5606104a9f",
     whatsapp: "5188679538"
